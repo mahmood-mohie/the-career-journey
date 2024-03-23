189c0a5c09a7f06675f36b665ffbfdd111508bcf
maahmood-mohie <maahmood.code@gmail.com>
2024-03-23 17:59:24 +0200

    create and style header menu for mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,22 @@
 let header = document.querySelector("#header")
 let hamburderMenu = document.querySelector("#hamburderMenu")
 let body = document.querySelector("body")
+let fadeElements = document.querySelectorAll(".has-fade")
 
 hamburderMenu.addEventListener("click", function() {
-    header.classList.toggle("open")
-    body.classList.toggle("noscroll")
+    if (header.classList.contains("open")) {
+        header.classList.remove("open")
+        fadeElements.forEach((e)=> {
+            e.classList.remove("fade-in")
+            e.classList.add("fade-out")
+        })
+        body.classList.remove("noscroll")
+    } else {
+        header.classList.add("open")
+        fadeElements.forEach((e)=> {
+            e.classList.add("fade-in")
+            e.classList.remove("fade-out")
+        })
+        body.classList.add("noscroll")
+    }
 })
